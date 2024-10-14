45759e20da1938a91583850896b0f9224a53e492
shepherdmhlanga20 <shepherdmhlanga20@gmail.com>
2024-10-14 14:35:50 +0000

    Added limit to projects displayed on the home pages

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,14 @@
 import '../assets/styles/Projects.css';
 import { useEffect, useState } from 'react';
 import ProjectCard from '../components/ProjectCard';
+import { useLocation } from 'react-router-dom';
 
 const Projects = () => {
 
+  const location = useLocation();
+
   const [projects, setProjects] = useState([])
+  const [limit, setLimit] = useState(undefined)
 
   useEffect(()=>{
     function getProjects(){
@@ -12,19 +16,24 @@ const Projects = () => {
           .then(response => response.json())
           .then(data => {
               setProjects(data)
-              console.log(data)
           })
           .catch(error => console.error(error))
     }
 
+    if(location["pathname"] == "/"){
+      setLimit(2)
+    }else if(location["pathname"] == "/projects"){
+      setLimit(undefined)
+    }
+
     getProjects()
-  },[])
+  },[location])
 
   return (
     <div className="container-fluid projects-container">
       <h2>Projects</h2>
       <div className="row">
-        {projects.map((project, index) => (
+        {projects.slice(0,limit).map((project, index) => (
           <ProjectCard
             key={index}
             project={project}
